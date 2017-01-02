9803be9f2e4c0f4b8801d840b933829811824915
Trond Bråthen <tabrath@gmail.com>
2017-01-02 16:05:58 +0100

    readerwriterlockslim extension timeouts

---
+++ LibP2P.Utilities.Tests/ReaderWriterLockSlimTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using LibP2P.Utilities.Extensions;
+using NUnit.Framework;
+
+namespace LibP2P.Utilities.Tests
+{
+    [TestFixture]
+    public class ReaderWriterLockSlimTests
+    {
+        [Test]
+        public void Read_CanTimeout()
+        {
+            using (var rwls = new ReaderWriterLockSlim())
+            {
+                var task = Task.Factory.StartNew(() => rwls.Write(() => Thread.Sleep(100)));
+
+                Assert.Throws<TimeoutException>(() => rwls.Read(() => { }, 50));
+
+                task.Wait();
+            }
+        }
+
+        [Test]
+        public void Write_CanTimeout()
+        {
+            using (var rwls = new ReaderWriterLockSlim())
+            {
+                var task = Task.Factory.StartNew(() => rwls.Write(() => Thread.Sleep(100)));
+
+                Assert.Throws<TimeoutException>(() => rwls.Write(() => { }, 10));
+
+                task.Wait();
+            }
+        }
+    }
+}
--- LibP2P.Utilities/Extensions/ReaderWriterLockSlimExtensions.cs
+++ LibP2P.Utilities/Extensions/ReaderWriterLockSlimExtensions.cs
@@ -5,55 +5,63 @@ namespace LibP2P.Utilities.Extensions
 {
     public static class ReaderWriterLockSlimExtensions
     {
-        public static T Read<T>(this ReaderWriterLockSlim rw, Func<T> func)
+        public static void Read(this ReaderWriterLockSlim rwls, Action action, int timeout = Timeout.Infinite)
         {
-            rw.EnterReadLock();
-            try
-            {
-                return func();
-            }
-            finally 
-            {
-                rw.ExitReadLock();
-            }
-        }
+            if (!rwls.TryEnterReadLock(timeout))
+                throw new TimeoutException("Read deadlock");
 
-        public static void Read(this ReaderWriterLockSlim rw, Action action)
-        {
-            rw.EnterReadLock();
             try
             {
                 action();
             }
             finally
             {
-                rw.ExitReadLock();
+                rwls.ExitReadLock();
             }
         }
 
-        public static T Write<T>(this ReaderWriterLockSlim rw, Func<T> func)
+        public static T Read<T>(this ReaderWriterLockSlim rwls, Func<T> func, int timeout = Timeout.Infinite)
         {
-            rw.EnterWriteLock();
+            if (!rwls.TryEnterReadLock(timeout))
+                throw new TimeoutException("Read deadlock");
+
             try
             {
                 return func();
             }
             finally
             {
-                rw.ExitWriteLock();
+                rwls.ExitReadLock();
             }
         }
 
-        public static void Write(this ReaderWriterLockSlim rw, Action action)
+        public static void Write(this ReaderWriterLockSlim rwls, Action action, int timeout = Timeout.Infinite)
         {
-            rw.EnterWriteLock();
+            if (!rwls.TryEnterWriteLock(timeout))
+                throw new TimeoutException("Write deadlock");
+
             try
             {
                 action();
             }
             finally
             {
-                rw.ExitWriteLock();
+                rwls.ExitWriteLock();
+            }
+        }
+
+        public static T Write<T>(this ReaderWriterLockSlim rwls, Func<T> func, int timeout = Timeout.Infinite)
+        {
+            if (!rwls.TryEnterWriteLock(timeout))
+                throw new TimeoutException("Write deadlock");
+
+            try
+            {
+                return func();
+            }
+            finally
+            {
+                rwls.ExitWriteLock();
             }
         }
     }
